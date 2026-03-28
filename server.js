6eb20ea1bfe83ea395167926137676d7122207df
LastZoneLegends <lastzonelegends0@gmail.com>
2026-03-28 15:42:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const express = require("express");
 const cors = require("cors");
 const axios = require("axios");
 const db = require("./firebase");
-admin.initializeApp();
+const admin = require("firebase-admin");
 
 const app = express();
 
